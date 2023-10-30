160951abb0ff2ddba690b1996287a2ae2ef91ed2
monzir3bdo <monizr3bdo@gmail.com>
2023-10-30 20:32:49 +0300

    AppBar Theme

--- lib/presentation/app_theme.dart
+++ lib/presentation/app_theme.dart
@@ -1,19 +1,34 @@
 import 'package:clean_architecture/presentation/colors_manager.dart';
+import 'package:clean_architecture/presentation/fonts_manager.dart';
+import 'package:clean_architecture/presentation/styles_manager.dart';
 import 'package:clean_architecture/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
   return ThemeData(
-      primaryColor: ColorsManager.primary,
-      primaryColorLight: ColorsManager.primaryWithOpacity70,
-      primaryColorDark: ColorsManager.darkGrey,
-      disabledColor: ColorsManager.grey1,
-      colorScheme:
-          ColorScheme.fromSwatch().copyWith(secondary: ColorsManager.grey),
-      //Card Theme
-      cardTheme: CardTheme(
-        color: Colors.white,
-        shadowColor: ColorsManager.grey,
-        elevation: AppSize.s4,
-      ));
+    primaryColor: ColorsManager.primary,
+    primaryColorLight: ColorsManager.primaryWithOpacity70,
+    primaryColorDark: ColorsManager.darkGrey,
+    disabledColor: ColorsManager.grey1,
+    colorScheme:
+        ColorScheme.fromSwatch().copyWith(secondary: ColorsManager.grey),
+    splashColor: ColorsManager.primaryWithOpacity70,
+    //Card Theme
+    cardTheme: CardTheme(
+      color: Colors.white,
+      shadowColor: ColorsManager.grey,
+      elevation: AppSize.s4,
+    ),
+    //Appbar Theme
+    appBarTheme: AppBarTheme(
+      backgroundColor: ColorsManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorsManager.primaryWithOpacity70,
+      centerTitle: true,
+      titleTextStyle: getRegularStyle(
+        color: ColorsManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
+  );
 }
